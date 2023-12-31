e10dad68e70a823e5cb7c0cd63d274e0f8920d3d
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-12-31 14:59:58 +0000

    chore: update testing framework to use common-tests repo

--- test.sh
+++ test.sh
@@ -4,58 +4,82 @@ RED='\033[0;31m'
 NC='\033[0m'
 GREEN='\033[0;32m'
 
-PY_ANALYSER_PATH="./src/py_analyser.py"
-TEST_DIR="./test"
+PY_ANALYSER="./src/py_analyser.py"
+TESTS_DIR="./test"
 
 normalize() {
     jq 'sort' | sed 's/"vulnerability": "\([A-Z]\)_[0-9]\+",/"vulnerability": "\1",/'
 }
 
-[ -d "output" ] || mkdir output
 
-if [ "$#" == 1 ]; then
-    SLICE_FILE="$TEST_DIR/slices/$1.py"
-    PATTERN_FILE="$TEST_DIR/patterns/$1.patterns.json"
-    OUTPUT_FILE="$TEST_DIR/outputs/$1.output.json"
-    PROGRAM_OUTPUT_FILE="./output/$1.output.json"
+if [ $# -eq 1 ]; then
+    TEST_DIR=$TESTS_DIR/$1
+    SLICE=$(find "$TEST_DIR" -type f -name '*.py')
+    PATTERN=$(find "$TEST_DIR" -type f -name '*.patterns.json')
+    OUTPUT_FILE=$(find "$TEST_DIR" -type f -name '*.output.json')
+    PROGRAM_OUTPUT_FILE=output/$(basename $OUTPUT_FILE)
 
-    python3 $PY_ANALYSER_PATH $SLICE_FILE $PATTERN_FILE > /dev/null 2>&1
+    python3 $PY_ANALYSER $SLICE $PATTERN > /dev/null 2>&1
+
+    if [ $? -ne 0 ]; then
+        echo -e "$1: ${RED}FAILED${NC}"
+        exit 0
+    fi
+
+    VALIDATE_OUTPUT=$(python3 $TESTS_DIR/validate.py -o $PROGRAM_OUTPUT_FILE -t $OUTPUT_FILE)
+
+    if [ $? -ne 0 ]; then
+        echo -e "$1: ${RED}FAILED${NC}"
+        exit 0
+    fi
 
     EXPECTED_OUTPUT=$(jq . "$OUTPUT_FILE" | normalize)
     PROGRAM_OUTPUT=$(jq . "$PROGRAM_OUTPUT_FILE" | normalize)
 
-    if diff <(echo "$PROGRAM_OUTPUT") <(echo "$EXPECTED_OUTPUT") &> /dev/null; then
-        echo -e "${GREEN}PASSED${NC}"
+    tt=$(echo "$VALIDATE_OUTPUT" | ansi2txt | tr -d '\n' | awk '/WRONG FLOWS\[\]/ && /MISSING FLOWS\[\]/')
+
+    if [ "$tt" != "" ]; then
+        echo -e "$1: ${GREEN}PASSED${NC}"
     else
-        echo -e "${RED}FAILED${NC}"
+        echo -e "$1: ${RED}FAILED${NC}"
         sdiff <(echo "$PROGRAM_OUTPUT" ) <(echo "$EXPECTED_OUTPUT")
     fi
 
     exit 0
 fi
 
-passed=0
-total=0
+PASSED=0
+TOTAL=0
+for test_dir in $(ls -d "$TESTS_DIR"/T*); do
+    TOTAL=$((TOTAL+1))
 
-for SLICE_FILE in $TEST_DIR/slices/*.py; do
-    SLICE_NAME=$(basename $SLICE_FILE .py)
-    PATTERN_FILE="$TEST_DIR/patterns/$SLICE_NAME.patterns.json"
-    OUTPUT_FILE="$TEST_DIR/outputs/$SLICE_NAME.output.json"
-    PROGRAM_OUTPUT_FILE="./output/$SLICE_NAME.output.json"
+    SLICE=$(find "$test_dir" -type f -name '*.py')
+    PATTERN=$(find "$test_dir" -type f -name '*.patterns.json')
+    OUTPUT_FILE=$(find "$test_dir" -type f -name '*.output.json')
+    PROGRAM_OUTPUT_FILE=output/$(basename $OUTPUT_FILE)
 
-    python3 $PY_ANALYSER_PATH $SLICE_FILE $PATTERN_FILE > /dev/null 2>&1
+    python3 $PY_ANALYSER $SLICE $PATTERN > /dev/null 2>&1
 
-    EXPECTED_OUTPUT=$(jq . "$OUTPUT_FILE" | normalize)
-    PROGRAM_OUTPUT=$(jq . "$PROGRAM_OUTPUT_FILE" | normalize)
-
-    if diff <(echo "$PROGRAM_OUTPUT") <(echo "$EXPECTED_OUTPUT") &> /dev/null; then
-        echo -e "${GREEN}PASSED: ${NC}$SLICE_NAME"
-        passed=$((passed+1))
-    else
-        echo -e "${RED}FAILED: ${NC}$SLICE_NAME"
+    if [ $? -ne 0 ]; then
+        echo -e "$(basename $test_dir): ${RED}FAILED${NC}"
+        continue
     fi
-    total=$((total+1))
+
+    VALIDATE_OUTPUT=$(python3 $TESTS_DIR/validate.py -o $PROGRAM_OUTPUT_FILE -t $OUTPUT_FILE)
+
+    if [ $? -ne 0 ]; then
+        echo -e "$(basename $test_dir): ${RED}FAILED${NC}"
+        continue
+    fi
+
+    tt=$(echo "$VALIDATE_OUTPUT" | ansi2txt | tr -d '\n' | awk '/WRONG FLOWS\[\]/ && /MISSING FLOWS\[\]/')
+
+    if [ "$tt" != "" ]; then
+        echo -e "$(basename $test_dir): ${GREEN}PASSED${NC}"
+        PASSED=$((PASSED+1))
+    else
+        echo -e "$(basename $test_dir): ${RED}FAILED${NC}"
+    fi
 done
 
-echo
-echo "PASSED: $passed/$total"
+echo -e "\nPASSED: ${GREEN}$PASSED/$TOTAL${NC}"
